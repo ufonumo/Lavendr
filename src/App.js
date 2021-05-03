1ab6543d2b93c45aea88248c619013dd6c6b7b33
ufonumo <ufonumo@gmail.com>
2021-05-03 17:26:22 +0100

    lavendr

--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
 import './App.css';
 import Movie from './component/Movie';
+import React , {useEffect, useState} from 'react'
+
+
 
 function App() {
   const FeatureaAPI = `https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc`;
   const ImageAPI = `https://image.tmdb.org/t/p/w500`;
   const SearchAPI = `https://api.themoviedb.org/3/search/movie?api_key=`
+  const [movies, setMovies] = useState([]);
+
+  useEffect(async () => {
+    
+    const response = await fetch(FeatureaAPI)
+    const moviesres = await response.json();
+    setMovies(moviesres)
+
+  }, [])
+
+  console.log(movies);
 
   return (
     <>
-      <Movie/>
+      {movies.map((list) =>(
+        <Movie/>
+      ) )}
     </>
   );
 }
--- src/component/Movie.js
+++ src/component/Movie.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Movie() {
     return (
         <>
-            
+            this is a movie template
         </>
     )
 }
