53ccf92970c97edd7805ca920e0911a7a9071f5e
ufonumo <ufonumo@gmail.com>
2021-05-17 17:55:33 +0100

    lavendr

--- src/App.js
+++ src/App.js
@@ -5,26 +5,48 @@ import React , {useEffect, useState} from 'react'
 
 
 function App() {
-  const FeatureaAPI = `https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc`;
-  const ImageAPI = `https://image.tmdb.org/t/p/w500`;
-  const SearchAPI = `https://api.themoviedb.org/3/search/movie?api_key=`
+  const APIKEY = `713b06f463955d2d504de27d17e4f655`;
+  const FeatureaAPI = `https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=${APIKEY}`;
+  const SearchAPI = `https://api.themoviedb.org/3/search/movie?api_key=${APIKEY}&query=`;
+  // const TOKEN = `eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3MTNiMDZmNDYzOTU1ZDJkNTA0ZGUyN2QxN2U0ZjY1NSIsInN1YiI6IjYwOTAwZWNlYTE5OWE2MDAyOTg2ZTJiYSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.yn524nOINkj43kOfNYsVBNDz-9Q4Ffx1-RCB3Ufkddg`;
+  
   const [movies, setMovies] = useState([]);
+  const [searchMovie, setSearchMovie] = useState('');
 
-  useEffect(async () => {
-    
-    const response = await fetch(FeatureaAPI)
-    const moviesres = await response.json();
-    setMovies(moviesres)
-
+  useEffect( () => {
+    FetchMovies(FeatureaAPI);
   }, [])
 
-  console.log(movies);
+  const FetchMovies = async (APIKEY) =>{
+    const response = await fetch(APIKEY)
+    const moviesres = await response.json();
+    setMovies(moviesres.results)
+    console.log(moviesres);
+  }
+
+  const handleSubmit = (e) =>{
+    e.preventDefault();
+
+    FetchMovies(SearchAPI + searchMovie)
+  }  
+
+  const handleChange = (e) =>{
+    setSearchMovie(e.target.value)
+  }
 
   return (
     <>
-      {movies.map((list) =>(
-        <Movie/>
-      ) )}
+      <header className="header">
+        <form onSubmit={handleSubmit}>
+          <input onChange={handleChange}  type="text" value={searchMovie} 
+          placeholder='Search...' className="search_bar" />
+        </form>
+      </header>
+      <div className='movie_container'>
+        {movies.length > 0 && movies.map((list) =>(
+          <Movie data={list} key={list.id}/>
+        ) )}
+      </div>
     </>
   );
 }
--- src/component/Movie.js
+++ src/component/Movie.js
@@ -1,9 +1,34 @@
-import React from 'react'
+import React from 'react';
+import dummy from '../assets/pic.jpg'
 
-export default function Movie() {
+export default function Movie({data}) {
+    const ImageAPI = `https://image.tmdb.org/t/p/w500`;
+    const setRatings = (ratings) =>{
+        if(ratings >= 8){
+            return 'green'
+        }
+        else if(ratings >= 6){
+            return 'orange'
+        }
+        else{
+            return 'red'
+        }
+    }
+
     return (
-        <>
-            this is a movie template
+        < >
+            <div className='movie'>
+                <img src={ (data.poster_path ? ImageAPI + data.poster_path : dummy) } alt={data.title}/>
+                <div className="movie_info">
+                    <h3>{data.title}</h3>
+                    <h4 className={`rates ${setRatings(data.vote_average)}`} >{data.vote_average}</h4>
+                </div>
+
+                <div className="movie_overview">
+                    <h2>Overview:</h2>
+                    <p>{data.overview}</p>
+                </div>
+            </div>
         </>
     )
 }
